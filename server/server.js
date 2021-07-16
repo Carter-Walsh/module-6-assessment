68659dd363f42c6ebfd1631fa5b09e1c6e01d7f4
Carter Walsh <Cwalsh616@gmail.com>
2021-07-16 11:11:30 -0500

    Fixed server.js pathway

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static("public"));
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, "./tictacjs.html"));
+  res.sendFile(path.join(__dirname, "../tictacjs.html"));
 });
 
 const port = process.env.PORT || 4000
